7064686b75c764b10077d81a173057fbb6f465e9
Jeff Yu <jwy5140@gmail.com>
2019-05-07 21:14:06 -0400

    fixed map

--- ericgame.js
+++ ericgame.js
@@ -2,22 +2,28 @@ var player;
 var platforms; 
 var cursors;
 
-    function preload()
+class First extends Phaser.Scene{
+    constructor()
+    {
+        super("First")
+    }
+
+    preload()
     {
 
         this.load.image('back', 'Assets/country-platform-files/layers/country-platform-back.png');
-        this.load.image('forest', 'Assets/country-platform-files/layers/country-platform-forest.png');
-        this.load.image('set', 'Assets/country-platform-files/country-platform.psd')
-        this.load.spritesheet('eric', 'Assets/eric.png', {frameWidth: 32, frameHeight: 32})
+        this.load.image('forest', 'Assets/country-platform-files/layers/country-platform-tiles-example.png');
+        this.load.image('set', 'Assets/country-platform-files/layers/country-platform-tileset.png');
+        this.load.spritesheet('eric', 'Assets/eric.png', {frameWidth: 32, frameHeight: 32});
         
     }
 
-    function create()
+    create()
     {
-        this.add.image(192, 112,'back')
-        player = this.physics.add.sprite(225, 0, 'eric').setScale(1.2)
-        platforms = this.physics.add.staticGroup()
-        platforms.create(150, 112, 'forest').setScale(1, .2).refreshBody();
+        this.bg = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'back').setOrigin(0);
+        player = this.physics.add.sprite(0, 0, 'eric');
+        platforms = this.physics.add.staticGroup();
+        platforms.create(80, 200, 'forest').setScale(.25).refreshBody();
         this.physics.add.collider(player, platforms);
 		player.setCollideWorldBounds(true);
         cursors = this.input.keyboard.createCursorKeys();
@@ -41,10 +47,87 @@ var cursors;
             frameRate: 6,
             repeat: -1
         });
+
+
     }
 
-    function update()
+    update()
     {
+        // this.bg.tilePosition.X += .5;
+
+        if (cursors.left.isDown) {
+            player.setVelocityX(-160);
+            player.anims.play('left', true)
+		} else if (cursors.right.isDown) {
+            player.setVelocityX(160);
+            player.anims.play('right', true)
+		} else {
+            player.setVelocityX(0);
+            player.anims.play('turn')
+        }
+        if (cursors.space.isDown) {
+            player.setVelocityY(-160);
+        }
+        // if(this.player.sprite.x > 15400){
+        //     this.scene.end(First);
+        //     this.scene.start(Second);
+        // }    
+    }
+}
+
+class Second extends Phaser.Scene{
+    constructor()
+    {
+        super("Second")
+    }
+
+    preload()
+    {
+
+        this.load.image('back', 'Assets/country-platform-files/layers/country-platform-back.png');
+        this.load.image('forest', 'Assets/country-platform-files/layers/country-platform-forest.png');
+        this.load.image('set', 'Assets/country-platform-files/country-platform.psd')
+        this.load.spritesheet('eric', 'Assets/eric.png', {frameWidth: 32, frameHeight: 32})
+        
+    }
+
+    create()
+    {
+        this.bg = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'back').setOrigin(0);
+        player = this.physics.add.sprite(0, 0, 'eric')
+        platforms = this.physics.add.staticGroup()
+        platforms.create(150, 112, 'set').setScale(1,1).refreshBody();
+        this.physics.add.collider(player, platforms);
+		player.setCollideWorldBounds(true);
+        cursors = this.input.keyboard.createCursorKeys();
+        
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('eric', { start: 0, end: 2 }),
+            frameRate: 6,
+            repeat: -1
+        });
+    
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'eric', frame: 3 } ],
+            frameRate: 20
+        });
+    
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('eric', { start: 4, end: 6 }),
+            frameRate: 6,
+            repeat: -1
+        });
+
+
+    }
+
+    update()
+    {
+        this.bg.tilePosition.X += .5;
+
         if (cursors.left.isDown) {
             player.setVelocityX(-160);
             player.anims.play('left', true)
@@ -59,12 +142,16 @@ var cursors;
             player.setVelocityY(-160);
         }
     }
-
+}
 
 const config = {
 	type: Phaser.AUTO,
-	width: 384,
-	height: 224,
+	width: 1024,
+    height: 768/3.5,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
 	backgroundColor: "b9eaff",
 	physics: {
 		default: 'arcade',
@@ -73,11 +160,8 @@ const config = {
 			enableBody: true,
 		}
 	},
-	scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+	scene: [First]
+    
 };
 
-const game = new Phaser.Game(config)
+const game = new Phaser.Game(config);
